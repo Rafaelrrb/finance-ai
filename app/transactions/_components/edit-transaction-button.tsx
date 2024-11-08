092844d780c8345f3f7e992e933defbfcc16745d
Rafaelrrb <rafaeborba@gmail.com>
2024-11-08 13:06:39 -0300

    feat: ensure transaction can be updated

---
+++ app/transactions/_components/edit-transaction-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+
+import { PencilIcon } from "lucide-react";
+import { Button } from "@/app/_components/ui/button";
+import UpsertTransactionDialog from "@/app/_components/upsert-transaction-dialog";
+import { Transaction } from "@prisma/client";
+
+interface EditTransactionDialogProps {
+  transaction: Transaction;
+}
+
+export default function EditTransactionButton({
+  transaction,
+}: EditTransactionDialogProps) {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="text-muted-foreground"
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        <PencilIcon />
+      </Button>
+      <UpsertTransactionDialog
+        isOpen={dialogIsOpen}
+        setIsOpen={setDialogIsOpen}
+        defaultValues={{
+          ...transaction,
+          amount: Number(transaction.amount),
+        }}
+        transactionId={transaction.id}
+      />
+    </>
+  );
+}
