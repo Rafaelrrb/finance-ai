62d23380f9d8709b5d967627b1da64787d1a3436
Rafaelrrb <rafaeborba@gmail.com>
2024-11-12 13:18:15 -0300

    chore: configure standalone output in next.config.js for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone", // Configuração para otimizar o uso do Prisma em Vercel
+  // Outras configurações opcionais:
+  experimental: {
+    appDir: true, // Caso esteja usando o diretório 'app' do Next.js
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
